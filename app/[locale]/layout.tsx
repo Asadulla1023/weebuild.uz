415968f63750fddc5eeca07b0e1b85876df087f2
Asadulloh <sayfullayevasadulla1@gmail.com>
2024-03-02 14:23:06 +0500

    test time

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,18 +1,18 @@
-import {NextIntlClientProvider} from 'next-intl';
-import {notFound} from 'next/navigation';
+import { NextIntlClientProvider } from 'next-intl';
+import { notFound } from 'next/navigation';
 import "@/styles/globals.css"
 export function generateStaticParams() {
-  return [{locale: 'ru'}, {locale: 'uz'}];
+  return [{ locale: 'ru' }, { locale: 'uz' }];
 }
- 
-export default async function LocaleLayout({children, params: {locale}}:any) {
+
+export default async function LocaleLayout({ children, params: { locale } }: any) {
   let messages;
   try {
     messages = (await import(`../../messages/${locale}.json`)).default;
   } catch (error) {
     notFound();
   }
- 
+
   return (
     <html lang={locale}>
       <head>
@@ -21,10 +21,19 @@ export default async function LocaleLayout({children, params: {locale}}:any) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/logo.png" />
       </head>
-      <body>
-        <NextIntlClientProvider locale={locale} messages={messages}>
+      <body style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        maxHeight:"100%"
+      }}>
+        {/* <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
-        </NextIntlClientProvider>
+        </NextIntlClientProvider> */}
+        <h1 style={{
+          textAlign: "center",
+        }}>{locale === "uz" ? "Sayt vaqtincha ishlab chiqish rejimida" : "Сайт временно находится в режиме разработки"}</h1>
       </body>
     </html>
   );
